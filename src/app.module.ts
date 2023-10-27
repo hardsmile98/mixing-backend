3570c7e9076a251b9772f1893a593ac89981c796
Денис Котелев <d.kotelev@carprice.ru>
2023-10-27 14:21:03 +0300

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { MixingModule } from './mixing/mixing.module';
 import { LettersModule } from './letters/letters.module';
 import { BitcoinModule } from './bitcoin/bitcoin.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MixingModule, LettersModule, BitcoinModule],
+  imports: [
+    MixingModule,
+    LettersModule,
+    BitcoinModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
--- src/bitcoin/bitcoin.service.ts
+++ src/bitcoin/bitcoin.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+const Client = require('bitcoin-core');
 
 @Injectable()
 export class BitcoinService {
+  client = null;
+
+  constructor(private readonly configService: ConfigService) {
+    const network = this.configService.get<string>('BITCOIN_NETWORK');
+    const host = this.configService.get<string>('BITCOIN_HOST');
+    const port = this.configService.get<number>('BITCOIN_PORT');
+    const username = this.configService.get<string>('BITCOIN_USERNAME');
+    const password = this.configService.get<string>('BITCOIN_PASSWORD');
+
+    this.client = new Client({
+      network,
+      host,
+      port,
+      username,
+      password,
+    });
+  }
+
   async encypt() {
-    return { ctypt: true };
+    try {
+      const info = await this.client.listWallets();
+      return { info };
+    } catch (e) {
+      console.log(e);
+      return {};
+    }
   }
 }
