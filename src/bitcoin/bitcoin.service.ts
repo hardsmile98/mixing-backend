8db9047a924b6068debefb80214d5ebab96e5b7a
Денис Котелев <d.kotelev@carprice.ru>
2023-10-27 16:37:14 +0300

    feat

--- src/bitcoin/bitcoin.service.ts
+++ src/bitcoin/bitcoin.service.ts
@@ -1,36 +1,92 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 const Client = require('bitcoin-core');
 
 @Injectable()
-export class BitcoinService {
+export class BitcoinService implements OnModuleInit {
   mainClient = null;
+  letterLabel = 'letter';
+
+  config = {
+    network: this.configService.get<string>('BITCOIN_NETWORK'),
+    host: this.configService.get<string>('BITCOIN_HOST'),
+    port: this.configService.get<number>('BITCOIN_PORT'),
+    username: this.configService.get<string>('BITCOIN_USERNAME'),
+    password: this.configService.get<string>('BITCOIN_PASSWORD'),
+  };
+
+  wallet = this.configService.get<string>('BITCOIN_MAIN_WALLET');
 
   constructor(private readonly configService: ConfigService) {
-    const network = this.configService.get<string>('BITCOIN_NETWORK');
-    const host = this.configService.get<string>('BITCOIN_HOST');
-    const port = this.configService.get<number>('BITCOIN_PORT');
-    const username = this.configService.get<string>('BITCOIN_USERNAME');
-    const password = this.configService.get<string>('BITCOIN_PASSWORD');
-    const wallet = this.configService.get<string>('BITCOIN_MAIN_WALLET');
-
     this.mainClient = new Client({
-      network,
-      host,
-      port,
-      username,
-      password,
-      wallet,
+      ...this.config,
+      wallet: this.wallet,
     });
   }
 
-  async encypt() {
+  async getMainWalletAddressByLabel(label: string) {
     try {
-      const info = await this.mainClient.listWallets();
-      return { info };
+      const addressesByLabel = await this.mainClient.getAddressesByLabel(label);
+      return Object.keys(addressesByLabel || {});
     } catch (e) {
-      console.log(e);
-      return {};
+      return [];
     }
+  }
+
+  async getMainWalletNewAddress(label?: string, type?: string) {
+    try {
+      const addressesByLabel = await this.mainClient.getNewAddress(label, type);
+      return addressesByLabel;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  async getLetterAddress() {
+    const addresses = await this.getMainWalletAddressByLabel(this.letterLabel);
+
+    if (!addresses.length) {
+      return null;
+    }
+
+    return addresses[0];
+  }
+
+  // if the main wallet is missing, it creates
+  async createMainWallet() {
+    const wallets = await this.mainClient.listWallets();
+
+    if (!wallets.includes(this.wallet)) {
+      await this.mainClient.createWallet(this.wallet);
+
+      this.mainClient = new Client({
+        ...this.config,
+        wallet: this.wallet,
+      });
+    }
+  }
+
+  // if the letter adress is missing, it creates
+  async createLetterAddress() {
+    const letterAddress = await this.getLetterAddress();
+
+    if (letterAddress) {
+      return;
+    }
+
+    await this.getMainWalletNewAddress(this.letterLabel, 'legacy');
+  }
+
+  async onModuleInit() {
+    await this.createMainWallet();
+    await this.createLetterAddress();
+  }
+
+  async signMessage(message: string) {
+    const letterAddress = await this.getLetterAddress();
+
+    const signature = await this.mainClient.signMessage(letterAddress, message);
+
+    return signature;
   }
 }
--- src/letters/letters.service.ts
+++ src/letters/letters.service.ts
@@ -7,7 +7,7 @@ export class LettersService {
   constructor(private bitcoinService: BitcoinService) {}
 
   async getLetter(query: GetLetterQueryDto) {
-    return this.bitcoinService.encypt();
+    return this.bitcoinService.signMessage('message');
   }
 
   async checkLetter(dto: CheckLetterDto) {
