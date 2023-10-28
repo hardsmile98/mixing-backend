dcb5e12ed5643bea10269619336a21a2115d4559
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-10-28 10:49:51 +0300

    feat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MixingModule } from './mixing/mixing.module';
 import { LettersModule } from './letters/letters.module';
 import { BitcoinModule } from './bitcoin/bitcoin.module';
-import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     MixingModule,
     LettersModule,
     BitcoinModule,
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
